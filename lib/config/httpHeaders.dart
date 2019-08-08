0d732e17d2fe663bce3276f30e722506b3aaff1e
kajiecy <851416347@qq.com>
2019-08-08 13:32:46 +0800

    伪造请求头

---
+++ lib/config/httpHeaders.dart
@@ -0,0 +1,13 @@
+const httpHeaders = {
+"Accept": "application/json, text/plain, */*",
+"Accept-Encoding": "gzip, deflate, br",
+"Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+"Connection": "keep-alive",
+"Content-Length": 2,
+"Content-Type": "application/json",
+"Cookie": "_ga=GA1.2.1540422965.1565232807; _gid=GA1.2.1169881849.1565232807; _gat=1; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1565232807,1565240689; SERVERID=3431a294a18c59fc8f5805662e2bd51e|1565240693|1565240688; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1565240694",
+"Host": "time.geekbang.org",
+"Origin": "https://time.geekbang.org",
+"Referer": "https://time.geekbang.org/",
+"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import './../config/httpHeaders.dart';
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-class HomePage extends StatelessWidget {
+class _HomePageState extends State<HomePage> {
+  String showText = '请求远程数据';
   @override
   Widget build(BuildContext context) {
-    getHttp();
-    return Scaffold(
-      body: Center(
-        child: Text('首页'),
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('请求远程数据'),
+        ),
+          body:SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: _jike,
+                  child: Text('请求数据'),
+                ),
+                Text(showText)
+              ],
+            ),
+          )
+
       ),
     );
   }
-  void getHttp() async{
+  void _jike(){
+    print('开始向极客时间请求数据..............');
+    getHttp().then((val){
+      setState(() {
+        showText = val['data'].toString();
+      });
+    });
+  }
+  Future getHttp() async{
     try{
       Response response;
-      response = await Dio().get("https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=卡杰");
-      return print(response);
+      Dio dio = Dio();
+      print('得到的headers为');
+      print(httpHeaders['User-Agent']);
+      dio.options.headers = httpHeaders;
+      response = await dio.get('https://time.geekbang.org/serv/v1/column/newAll');
+      print(response);
+      return response;
     }catch(e){
       return print(e);
     }
