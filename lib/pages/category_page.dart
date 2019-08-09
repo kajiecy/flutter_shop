048030adeb680f7eedebe66043f7f5774370d0e7
kajiecy <851416347@qq.com>
2019-08-09 13:59:45 +0800

    分类界面左侧

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,13 +1,83 @@
 import 'package:flutter/material.dart';
+import './../service/service_method.dart';
+import './../model/category.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('分类'),
+      appBar: AppBar(
+        title: Text('商品分类'),
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+          ],
+        ),
       ),
     );
   }
 }
 
+//左侧大类导航
+class LeftCategoryNav extends StatefulWidget {
+  @override
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List<CategoryModel> categoryModelList = [];
+  int currentIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+          border: Border(right: BorderSide(width: 0.5, color: Colors.black12))),
+      child: ListView.builder(
+        itemCount: categoryModelList.length,
+        itemBuilder: (context, index) {
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCategoryList();
+  }
+
+  Widget _leftInkWell(int index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10, top: 20),
+        decoration: BoxDecoration(
+            color: this.currentIndex == index ? Colors.black12 : Colors.white,
+            border:
+                Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+        child: Text(
+          categoryModelList[index].mallCategoryName,
+          style: TextStyle(
+            fontSize: ScreenUtil().setSp(26),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _getCategoryList() async {
+    await request('getCategory').then((val) {
+      setState(() {
+        categoryModelList = getCategoryModelList(val.toString());
+        print(categoryModelList);
+      });
+    });
+  }
+}
